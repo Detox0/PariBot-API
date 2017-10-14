2a3363823b92d37324f5ff66709c5d6b1af363de
Gerardo Zúñiga <monodeth@gmail.com>
2017-10-14 19:58:20 -0300

    Implemented requests to bot api

--- API/urls.py
+++ API/urls.py
@@ -4,6 +4,7 @@ from API import views
 urlpatterns = [
     url(r'^users/$', views.user_list),
     url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
-    url(r'^message/$', views.receive_message),
-    url(r'^message/(?P<pk>[0-9]+)/$', views.all_user_messages),
+url(r'^users/(?P<pk>[0-9]+)/messages$', views.all_user_messages),
+    url(r'^messages/$', views.receive_message),
+
 ]
